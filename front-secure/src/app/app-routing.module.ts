64968d41dc5a27dda23c155bb22600506a23f0ce
carry <carrycharge@gmail.com>
2021-06-07 10:47:34 +0200

    profile page

--- front-secure/src/app/app-routing.module.ts
+++ front-secure/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
-import {AppComponent} from "./app.component";
 import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
